8dbc552ad0adf2f248713dcba32835e3ca83aa0c
Douglas Greyling <greyling.douglas@gmail.com>
2025-09-17 11:52:20 +0200

    crimesjohnson: Finish week 3

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+const baseController = require("./controllers/baseController")
+const inventoryRoute = require("./routes/inventoryRoute")
 
 /* ***********************
  * View Engine and Templates
@@ -23,6 +25,8 @@ app.set("layout", "./layouts/layout")
  * Routes
  *************************/
 app.use(static)
+app.get("/", baseController.buildHome)
+app.use("/inv", inventoryRoute)
 
 /* ***********************
  * Local Server Information
